3023a210a7a687024d98d92049a9b2570ac7dbfe
上杉亮介 <kazusa7221@gmail.com>
2024-02-28 07:19:48 +0900

    abc236

---
+++ practice/abc236/c/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import bisect,collections,copy,heapq,itertools,math,numpy,string;
+import sys
+def SI(): return int(sys.stdin.readline().rstrip())
+def MI(): return map(int, sys.stdin.readline().rstrip().split())
+def LI(): return list(map(int, sys.stdin.readline().rstrip().split()))
+def SS(): return sys.stdin.readline().rstrip()
+def MS(): return map(str, sys.stdin.readline().rstrip().split())
+def LS(): return list(sys.stdin.readline().rstrip().split())
+
+# N = SI()                         # 一つの数字
+# A,B = MI()                      # 空白区切りで複数の数字が与えられ、それらを別々の変数に格納したい時
+# S = MS()                        # 複数の文字列を空白区切りで与えられた時
+# A = LI()                        # シンプルに数列一行を読み込む
+# A = [LI() for _ in range(N)]    # N行の数字列を二次元配列に
+from collections import defaultdict
+N, M = MI()
+S = LS()
+T = LS()
+
+is_stop = defaultdict(int)
+for stop in T:
+    is_stop[stop] += 1
+
+for station in S:
+    if station in is_stop:
+        print('Yes')
+    else:
+        print('No')
